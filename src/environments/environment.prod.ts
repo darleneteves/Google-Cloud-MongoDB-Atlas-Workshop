7d5e0a2abffd5aa3af488cf139f9ff1660e8eb6c
darleneteves <157785122+darleneteves@users.noreply.github.com>
2024-01-26 15:34:55 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  APP_ID: '',
-  GRAPHQL_URI: '',
-  API_KEY: ''
+  APP_ID: 'bakery-wquma',
+  GRAPHQL_URI: 'https://asia-south1.gcp.realm.mongodb.com/api/client/v2.0/app/bakery-wquma/graphql',
+  API_KEY: 'JMFwOnhudXi9d2DlVlmlXvSyibfuU4yankjmRsaZhyvr0vr528cq4GDzZg6RhYpV'
 };
